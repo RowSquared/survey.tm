8f1a5fbb827abf0fe6e9d73d61c01fa147924823
Peter <p.brueckmann@mailbox.org>
2022-10-10 17:54:23 +0200

    1st draft get_suso functions

---
+++ R/get_suso.R
@@ -0,0 +1,133 @@
+
+#' Read SurSol Translation File
+#'
+#' @param qxid Id of Questionnaire in SuSo Designer
+#' @param user SuSo User Name
+#' @param password SuSo Password
+#' @param sheets Sheets from Translation to read. Default all
+#' @param types Which type of text items to keep
+#' @importFrom httr GET authenticate
+#' @importFrom readxl excel_sheets
+#' @import data.table
+get_sursol_titems_byqx <- function(qxid=NULL,
+                                   user="",
+                                   password="",
+                                   sheets=NULL,
+                                   types=c("Title","Instruction","OptionTitle","ValidationMessage",
+                                           "SpecialValue")) {
+  #TODO: ASSERT INPUT
+  #TODO: MAKE PROPER ERROR MESSAGES IF NOT 200/201 RETURN.
+
+  #BUILD URL
+  url <- paste0("https://designer.mysurvey.solutions/translations/",qxid,"/template")
+
+  #GET THE TRANSLATION FILE
+  request <- httr::GET(url=url,
+                       httr::authenticate(user,
+                                    password))
+  #WRITE TO TEMPFILE
+  tmp.file <- tempfile(fileext = '.xlsx')
+  writeBin(request$content, tmp.file)
+
+  #IDENTIFY ALL SHEETS
+  sheets.file <- readxl::excel_sheets(tmp.file)
+  if (!is.null(sheets)) {
+    #TODO: CHECK IF SHEETS INDICATED ARE ACTUALLY IN THE NAMES
+
+  }
+
+  #READ ALL SHEETS INTO ONE DT- ONLY COLS OF INTEREST
+  dt <- data.table::rbindlist(
+    lapply(sheets.file,\(sheet) {
+      data.table::as.data.table(readxl::read_excel(path=tmp.file,
+                               sheet=sheet))[,Type:=as.character(Type)][Type %chin% types | is.na(Type),
+                                                                        .(type=Type,
+                                                                          value=`Original text`)]
+
+    }))
+
+  #CREATE UNIQUE VALUE
+  cleanup.text.item(dt)
+
+  #GET ROW IDENTIFIER
+  dt[,seq.id:=1:.N]
+
+
+  return(dt)
+
+}
+
+
+
+
+
+#' Read SurSol Translation File
+#'
+#' @param questionnaires Named character vector of instruments to source. Elements must be id of questionnaire. Name is the Title of Questionnaire.
+#' @param user SuSo User Name
+#' @param password SuSo Password
+#' @param sheets Sheets from Translation to read. Default all
+#' @param types Which type of text items to keep
+#'
+#' @import data.table
+#' @importFrom httr GET authenticate
+#' @importFrom readxl excel_sheets
+#' @import data.table
+
+#' @return Data table of unique text items in instrument(s)
+#' @export
+#'
+get_sursol_titems <- function(questionnaires=NULL,
+                              user="",
+                              password="",
+                              sheets=NULL,
+                              types=c("Title","Instruction","OptionTitle","ValidationMessage",
+                                      "SpecialValue")) {
+
+  #TODO: ASSERT THAT NAMED CHARACTER VECTOR
+  assertthat::assert_that(is.char.named.vector(questionnaires),msg="questionnaires is not named character vector")
+
+  #GET ALL QUESTIONNAIRE ITEMS AND ALL SHEETS IN ONE DT
+  dt <-  rbindlist(lapply(questionnaires,\(x) {
+    get_sursol_titems_byqx(qxid=x,
+                           user=user,
+                           password=password,
+                           sheets=sheets,
+                           types=types)[,instrt:=names(questionnaires)[questionnaires %in% x]]
+  }))
+
+  #COLLAPSE
+  #TODO: PUT INTO FUNCTION AS REUSED FOR ODK
+  #KEEP UNIQUE: FIRST BY INSTRUMENT; VARIABLE AND VALUE TO AVOID HAVING MULTIPLE "label, label, label"
+  #ALWAYS ACCOUNT FOR SEQUENTIAL ID
+  dt <- dt[
+    ,.(
+      seq.id=seq.id[c(1)],
+      value=value[c(1)]
+    ),
+    by=.(instrt,type,value.unique)]
+  #NOW BY TYPE AND VALUE
+  dt <- dt[
+    ,.(
+      seq.id=seq.id[c(1)],
+      instrt=paste(instrt,collapse="\n"),
+      value=value[c(1)]
+    ),
+    by=.(type,value.unique)]
+
+  #NOW BY UNIQUE VALUE, WITH VARIABLE COLLAPSED
+  setorder(dt,value.unique,seq.id)
+  dt <- dt[,.(
+    seq.id=seq.id[c(1)],
+    instrt=instrt[c(1)],
+    type=paste(type,collapse="\n"),
+    value=value[c(1)]
+  ),by=.(value.unique)]
+  setorder(dt,seq.id)
+  #REMOVE SEQ ID HERE
+  dt[,seq.id:=NULL]
+
+  return(dt)
+
+}
+
