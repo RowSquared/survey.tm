36b6dbe2ecf0e2e71ca3ccdb65d916bd073bafac
Peter <p.brueckmann@mailbox.org>
2023-10-16 21:59:31 +0200

    add html-tag issue identifcation

--- R/check_software_mismatch.R
+++ R/check_software_mismatch.R
@@ -1,18 +1,78 @@
-
-#' Check individual sheet (data.table) for software mismatches
+#' Check individual sheet (data.table) for html-tag issues
 #'
 #' @param dt data table of a translation
-#' @param pattern pattern that identifies text item in Original/Translation (e.g. text substitution '%rostertitle%')
 #' @param lang name of sheet/language. for message
 #'
 #' @return data table with text items that contain issues
 #' @noRd
-get_sw_issues_dt <- function(dt,
+get_htmltag_issue_dt <- function(dt,
+                                 lang=NULL) {
+
+  # Extract tags function
+  html_tag_pattern <- "<(?:font color=\"[^\"]+\"|/?[a-zA-Z]+)>"
+  extract_tags <- function(text) {
+    unlist(regmatches(text, gregexpr(html_tag_pattern, text)))
+  }
+  # Count html tags
+  count_tags <- function(text) {
+    tags <- extract_tags(text)
+    table(tags)
+  }
+  #Run it
+  dt[, Text_Item_Tags := lapply(Text_Item, count_tags)]
+  dt[, Translation_Tags := lapply(Translation, count_tags)]
+
+
+  # Comparison function
+  compare_tags <- function(tags1, tags2) {
+    all(sort(names(tags1)) == sort(names(tags2)) &
+          all(tags1[sort(names(tags1))] == tags2[sort(names(tags2))]))
+  }
+
+  # Apply the comparison function to each row
+  dt[, Tags_Equivalent := mapply(compare_tags, Text_Item_Tags, Translation_Tags)]
+
+  #Keep rows with issues and apply a comment
+  # Convert the two list column to a character column
+  paste.list <- function(tags) paste(paste(names(tags), tags, sep="="), collapse="; ")
+  dt[Tags_Equivalent==FALSE,
+       comment.issue :=
+
+         paste0("Difference in count of html-tag:\n",
+           #Original/Text_Item
+           "Text_Item:",sapply(Text_Item_Tags, \(x) paste.list(x)),
+                "\n","Translation:",
+                sapply(Translation_Tags, \(x) paste.list(x)))
+
+    ]
+  #Keep cols & rows of interest (Just value.unique and comment.issue). Will be processed in parent function.
+   dt <- dt[Tags_Equivalent==FALSE,.(value.unique,
+                                  comment.issue)]
+
+
+  #Print results
+  message(paste(nrow(dt),"text items with html-tag issues for language",paste0("'",lang,"'")))
+
+  return(dt)
+}
+
+
+
+
+#' Check individual sheet (data.table) for text substitution issues
+#'
+#' @param dt data table of a translation
+#' @param pattern pattern that identifies Text substitution in Original/Translation (e.g. text substitution '%rostertitle%')
+#' @param lang name of sheet/language. for message
+#'
+#' @return data table with text items that contain issues
+#' @noRd
+get_txt_sub_issue_dt <- function(dt,
                            pattern="%[a-zA-Z0-9_]+%",
                            lang=NULL) {
 
   #Extract the text items per pattern into list column
-  dt <- dt[grepl(pattern,Text_Item),
+  dt <- dt[grepl(pattern,Text_Item)|grepl(pattern,Translation),
            .(orig=stringr::str_extract_all(Text_Item,pattern),
              trans=stringr::str_extract_all(Translation,pattern)
            ),by=.(value.unique)]
@@ -56,7 +116,7 @@ get_sw_issues_dt <- function(dt,
   dt <- dt[,.(comment.issue=paste(comment.issue, collapse = "\n")),by=.(value.unique)]
 
   #Print results
-  message(paste(nrow(dt),"text items with software-related mismatches for language",paste0("'",lang,"'")))
+  message(paste(nrow(dt),"text items with text substitution issues for language",paste0("'",lang,"'")))
 
   return(dt)
 }
@@ -79,17 +139,34 @@ identify_sw_issues <- function(
     tdb = list(),
     pattern="%[a-zA-Z0-9_]+%"
 ) {
-
+  # tdb <- new_tdb
   #Copy list to avoid replacement in place
   tdb <- copy(tdb)
 
-  #Identify issues for each translation
-  list.issues <- purrr::map(
+  #Identify Text Substitution issues for each translation - But the one in Status "outdated"
+  list.txtsub.issues <- purrr::map(
     .x = names(tdb),
-    .f = ~ get_sw_issues_dt(new_tdb[[.x]],
+    .f = ~ get_txt_sub_issue_dt(new_tdb[[.x]][!Status %chin% c("outdated")],
                                lang=.x)
   )
-  list.issues <- setNames(list.issues, names(tdb))
+  list.txtsub.issues <- setNames(list.txtsub.issues, names(tdb))
+
+  #Identify html-tag issues
+  list.html.issues <- purrr::map(
+    .x = names(tdb),
+    .f = ~ get_htmltag_issue_dt(new_tdb[[.x]][!Status %chin% c("outdated")],
+                                lang=.x)
+  )
+  list.html.issues <- setNames(list.html.issues, names(tdb))
+
+  #Bind both list of issues into one list
+  full.list.issues <- lapply(names(list.txtsub.issues), function(name) {
+    rbind(list.txtsub.issues[[name]], list.html.issues[[name]])
+  })
+  #Collapse to value.unique as an item could have both issues
+  collapse.operation <- function(dt) dt[,.(comment.issue=paste(comment.issue, collapse = "\n\n")),by=.(value.unique)]
+  full.list.issues <- lapply(full.list.issues, collapse.operation)
+  names(full.list.issues) <- names(list.txtsub.issues)
 
   #Update each Translation sheet
   updated.list <- lapply(names(tdb), \(x) {
@@ -98,10 +175,10 @@ identify_sw_issues <- function(
     tdb[[x]][, seq.id := 1:.N]
 
     #Update Status
-    tdb[[x]][value.unique %in%  list.issues[[x]]$value.unique,Status:="to update"]
+    tdb[[x]][value.unique %in%  full.list.issues[[x]]$value.unique,Status:="to be checked"]
 
     #Get in issues
-    tdb[[x]] <- merge(tdb[[x]],list.issues[[x]],by="value.unique",all.x=T)
+    tdb[[x]] <- merge(tdb[[x]],full.list.issues[[x]],by="value.unique",all.x=T)
 
     ###Update Comment###
     #Replace if Comment/Note empty
